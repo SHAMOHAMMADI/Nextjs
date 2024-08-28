6658ad708f89b58a8444563ad03760a038017dee
mustafa <shamohammadi64@gmail.com>
2024-08-28 09:39:35 +0330

    part1

---
+++ pages/Gallery/index.jsx
@@ -0,0 +1,17 @@
+// import React from 'react'
+
+// export default function Gallery() {
+//   return (
+//     <div>Gallery</div>
+//   )
+// }
+
+const Gallery = ()=>{
+    return(
+        <div>
+            <h2>Gallery</h2>
+        </div>
+    )
+}
+
+export default Gallery
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,24 @@
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+const style={
+  display:"flex",
+  color:"#0000ff",
+  justifyContent:"center",
+  height:"3em",
+  border:"none",
+  alignItems:'center',
+  boxShadow:"5px 1px 25px 1px rgba(1,1,1,0.2)",
+  margin:"10px 0",
+  borderRadius:"10px"
+
+}
+  return (
+  <>
+  <div style={style}>Header</div>
+  <Component {...pageProps} />
+  <div style={style}>footer</div>
+  </>
+  )
+  ;
 }
